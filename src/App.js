f281eb6b68e195cdfc93703ce998f7dd96fd8c20
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-31 13:28:38 +0530

    feat: Import components and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Login, Signup } from "./components";
+import {Home, Landing} from "./pages";
 
 
 function App() {
   return (
     <div className="App">
-     
+      {/* <Landing /> */}
+      {/* <Login /> */}
+      {/* <Signup /> */}
+      <Home />
     </div>
   );
 }
