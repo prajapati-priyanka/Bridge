30a9c1b086dd22571af89c98e864cb8d48a904c2
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-31 13:29:10 +0530

    feat: Add chakraProvider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from "./theme";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider resetCSS={true} theme={theme}>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
