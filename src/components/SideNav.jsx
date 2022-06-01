f6d044e64b3c92da534863b48d541b55fe5c7714
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-02 01:25:42 +0530

    feat: Create Sidebar

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,8 +1,40 @@
+import { Box, ListItem, List,ListIcon} from "@chakra-ui/react";
+import { AiOutlineHome,AiOutlineUser } from "react-icons/ai";
+import { MdOutlineExplore,MdOutlineBookmarkBorder } from "react-icons/md";
+
 
 
 const SideNav = () =>{
     return(
-  <div className=""></div>
+   <Box position="sticky" top="6rem" h="fit-content" w="18%" >
+       <List spacing={4} w="100%">
+           <ListItem  cursor="pointer" p="2" pl="10" fontSize="lg" borderRadius="20" _hover={{
+               bgColor:"#e6ddd2"
+           }} >
+               <ListIcon as ={AiOutlineHome} mb='0.5' mr="1rem" />
+               Feed
+               </ListItem>
+           <ListItem  cursor="pointer" p="2" pl="10" fontSize="lg" borderRadius="20" _hover={{
+               bgColor:"#e6ddd2"
+           }}>
+               <ListIcon as ={MdOutlineExplore} mb='0.5' mr="1rem" />
+               Explore
+               </ListItem>
+           <ListItem  cursor="pointer" p="2" pl="10" fontSize="lg" borderRadius="20" _hover={{
+               bgColor:"#e6ddd2"
+           }}>
+               <ListIcon as={MdOutlineBookmarkBorder} mb='0.5' mr="1rem" />
+               Bookmark
+               </ListItem>
+
+           <ListItem  cursor="pointer" p="2" pl="10" fontSize="lg" borderRadius="20" _hover={{
+               bgColor:"#e6ddd2"
+           }}>
+               <ListIcon as={AiOutlineUser} mb='0.5' mr="1rem" />
+               Profile
+               </ListItem>
+       </List>
+   </Box>
     )
 }
 
