6d8bf804b387bd31a678230d7dcd61ec0b51eb66
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-02 11:39:56 +0530

    feat: Change Box max-width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,25 +17,25 @@ const Home = () => {
       <Flex bg="var(--bg-color)" w="100%" gap="10" h="100%" pl="6" pr="6">
         <SideNav />
         <Flex flexDirection="column" gap="5" w="60%" mt="6rem">
-          <Box maxW="40rem" bg="white" p="4" borderRadius="20">
+          <Box maxW="45rem" bg="white" p="4" borderRadius="20">
             <PostCard />
           </Box>
-          <Box maxW="40rem" bg="white" p="4" borderRadius="20">
+          <Box maxW="45rem" bg="white" p="4" borderRadius="20">
             <PostCard />
           </Box>
-          <Box maxW="40rem" bg="white" p="4" borderRadius="20">
+          <Box maxW="45rem" bg="white" p="4" borderRadius="20">
             <PostCard />
           </Box>
-          <Box maxW="40rem" bg="white" p="4" borderRadius="20">
+          <Box maxW="45rem" bg="white" p="4" borderRadius="20">
             <PostCard />
           </Box>
-          <Box maxW="40rem" bg="white" p="4" borderRadius="20">
+          <Box maxW="45rem" bg="white" p="4" borderRadius="20">
             <PostCard />
           </Box>
-          <Box maxW="40rem" bg="white" p="4" borderRadius="20">
+          <Box maxW="45rem" bg="white" p="4" borderRadius="20">
             <PostCard />
           </Box>
-          <Box maxW="40rem" bg="white" p="4" borderRadius="20">
+          <Box maxW="45rem" bg="white" p="4" borderRadius="20">
             <PostCard />
           </Box>
         </Flex>
