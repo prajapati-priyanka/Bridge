d1fa54c6b172e7835fa0cc2b3e2800fa244640da
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-02 11:41:22 +0530

    feat: Create Bookmark page

---
+++ src/pages/Bookmark.jsx
@@ -0,0 +1,42 @@
+import { Box, Flex, useDisclosure, Heading } from "@chakra-ui/react";
+import {
+    Header,
+    PostCard,
+    SideNav,
+    SuggestedUsersSidebar,
+    UserProfileCard,
+  } from "../components"
+
+const Bookmark = ()=>{
+   
+        const {onOpen} = useDisclosure();
+        return (
+          <>
+            <Header onOpen={onOpen} />
+            <Flex bg="var(--bg-color)" w="100%" gap="10" h="100%" pl="6" pr="6">
+              <SideNav />
+              <Flex flexDirection="column" gap="5" w="60%" mt="6rem" >
+    
+
+                  
+                  <Box maxW="45rem" bg="white" p="4" borderRadius="20" mb="5">
+                    <PostCard />
+                  </Box>
+                  <Box maxW="45rem" bg="white" p="4" borderRadius="20" mb="5">
+                    <PostCard />
+                  </Box>
+                  <Box maxW="45rem" bg="white" p="4" borderRadius="20" mb="5">
+                    <PostCard />
+                  </Box>
+                  <Box maxW="45rem" bg="white" p="4" borderRadius="20" mb="5">
+                    <PostCard />
+                  </Box>
+               
+              </Flex>
+              <SuggestedUsersSidebar />
+            </Flex>
+          </>
+    )
+}
+
+export {Bookmark}
