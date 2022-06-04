5eb3864be448a4c701fdf2e2a7edd6d263829965
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-04 23:16:13 +0530

    refactor: Add extra space at the end

--- src/redux/asyncThunks/authThunk.js
+++ src/redux/asyncThunks/authThunk.js
@@ -3,10 +3,9 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const loginUser = createAsyncThunk("auth/login", async (newUser) => {
   const response = await axios.post("/api/auth/login", newUser);
-  console.log(response, "3rd")
+
   const data = { data: response.data, status: response.status };
   return data;
 });
 
-
-export { loginUser};
+export { loginUser };
