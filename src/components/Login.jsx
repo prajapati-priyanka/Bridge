76286a6639b94ce1170c052011e39908af88a104
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-04 18:51:28 +0530

    feat: Add Login Handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,15 +8,82 @@ import {
   InputRightElement,
   Button,
   Text,
-  Link
+  Link,
+  useToast
 } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { loginUser } from "../redux/asyncThunks/authThunk";
+import {useDispatch, useSelector} from "react-redux"
+
+
 
 const Login = () => {
-  const [show, setShow] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+ const toast = useToast();
+
+  const [newUser, setNewUser] = useState({
+    username: "",
+    password: "",
+  });
+  const [show, setShow] = useState(false);
+  const {isLoading} = useSelector(state=> state.auth)
+
   const handlePasswordClick = () => setShow(!show);
+
+  const guestUser = {
+    username: "pihu909",
+    password: "pihu7845",
+  };
+
+  const onChangeHandler = (e)=>{
+    const {name, value} = e.target;
+
+    setNewUser((prevUser => ({...prevUser, [name]:value})))
+}
+  
+  const guestLoginHandler = (e)=>{
+      e.preventDefault()
+      setNewUser(guestUser)
+  }
+
+  const checkInputs = () =>{
+    return newUser.username && newUser.password;
+  }
+
+  const loginHandler = async (e)=>{
+    if(checkInputs()){
+      e.preventDefault();
+       const response = await dispatch(loginUser(newUser));
+      if(response?.payload?.status===200){
+        localStorage.setItem("token", response.payload.data.encodedToken);
+        localStorage.setItem("user", JSON.stringify(response.payload.data.foundUser));
+      
+      }
+   
+      navigate(location?.state?.from?.pathname || "/home",{
+        replace:true,
+      });
+      toast({
+        description: "Successfully Logged In",
+        status: "success",
+        duration: 2000,
+        isClosable: true
+      })
+    }else{
+      toast({
+        description: "Enter both the fields",
+        status: "warning",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+   
+  }
+  
+  
 
   return (
     <Box
@@ -44,18 +111,24 @@ const Login = () => {
           rounded="none"
           variant="filled"
           type="email"
-          placeholder="Enter Email"
+          placeholder="Enter username"
+          name="username"
+          value={newUser.username}
+          onChange = {(e)=>onChangeHandler(e)}
         />
       </FormControl>
       <FormControl mb="3.5">
         <FormLabel>Password:</FormLabel>
         <InputGroup size="md">
           <Input
+           rounded="none"
+           variant="filled"
             pr="4.5rem"
             type={show ? "text" : "password"}
             placeholder="Enter password"
-            rounded="none"
-            variant="filled"
+            name="password"
+            value={newUser.password}
+            onChange = {(e)=>onChangeHandler(e)}
           />
           <InputRightElement width="4.5rem">
             <Button
@@ -70,10 +143,10 @@ const Login = () => {
         </InputGroup>
       </FormControl>
 
-      <Button variant="outline" display="block" w="100%" mt="8" mb="4">
+      <Button variant="outline" display="block" w="100%" mt="8" mb="4" onClick={guestLoginHandler}>
         Enter Guest Credentials
       </Button>
-      <Button variant="solid" display="block" w="100%" mb="4">
+      <Button variant="solid" display="block" w="100%" mb="4" onClick={loginHandler} isLoading={isLoading} loadingText="Logging In" spinnerPlacement="center">
         Login
       </Button>
 
