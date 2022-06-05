eeca1a65e92312344abed76207fd55d81ed4aa0a
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-05 12:45:17 +0530

    feat: Add signup cases

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { loginUser } from "../../redux/asyncThunks/authThunk";
+import { loginUser,signupUser } from "../../redux/asyncThunks/authThunk";
 
 const initialState = {
   user: JSON.parse(localStorage.getItem("user")) || null,
@@ -10,7 +10,14 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    logoutUser :(state)=>{
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem("user");
+      localStorage.removeItem("token")
+    }
+  },
   extraReducers: {
     [loginUser.pending]: (state, action) => {
       state.isLoading = true;
@@ -24,8 +31,24 @@ const authSlice = createSlice({
 
     [loginUser.rejected]: (state, action) => {
       state.isLoading = false;
+      console.error(action.error.message)
+    },
+    [signupUser.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+
+    [signupUser.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload.data.foundUser;
+      state.token = action.payload.data.encodedToken;
+    },
+
+    [signupUser.rejected]: (state, action) => {
+      state.isLoading = false;
+      console.error(action.error.message)
     },
   },
 });
 
-export default authSlice.reducer;
+export const {logoutUser} = authSlice.actions;
+export const {reducer:authReducer} = authSlice
