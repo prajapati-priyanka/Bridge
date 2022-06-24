13838ab5a81335f5deb91ef6ded878df22b2caec
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-24 15:08:42 +0530

    feat: Add postsReducer

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {authReducer } from "../slices";
+import {authReducer, postsReducer } from "../slices";
 
 export const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { auth: authReducer, posts: postsReducer },
 });
 
