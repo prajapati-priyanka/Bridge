e9cccbde25590a113cd8a366343d37ecdc1f857c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-24 14:58:02 +0530

    remove: Login and Signup

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export {Login} from "./Login";
-export {Signup} from "./Signup";
 export {SideNav} from "./SideNav";
 export {PostCard} from "./PostCard";
 export {CommentInput} from "./CommentInput";
