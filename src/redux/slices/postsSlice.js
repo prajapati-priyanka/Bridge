5f4c614807c81de8a5f988b624b7c1e71d4d6a06
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-24 15:06:30 +0530

    feat: Create postSlice

---
+++ src/redux/slices/postsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getAllPosts } from "../asyncThunks/postsThunk";
+
+const initialState = {
+    posts:[],
+    isLoading: false
+}
+
+const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {},
+    extraReducers:{
+        [getAllPosts.pending]: (state, action) => {
+            state.isLoading = true;
+          },
+      
+          [getAllPosts.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.posts = action.payload.data.posts;
+          
+          },
+      
+          [getAllPosts.rejected]: (state, action) => {
+            state.isLoading = false;
+            console.error(action.error.message)
+          },
+    }
+});
+
+export const {reducer: postsReducer} = postsSlice;
