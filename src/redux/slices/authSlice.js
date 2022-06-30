665c4bf8d07c99f6e36d8d1e41456b2d0a4fddca
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-06-30 12:43:51 +0530

    fix: Change foundUser to createdUser

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -39,7 +39,7 @@ const authSlice = createSlice({
 
     [signupUser.fulfilled]: (state, action) => {
       state.isLoading = false;
-      state.user = action.payload.data.foundUser;
+      state.user = action.payload.data.createdUser;
       state.token = action.payload.data.encodedToken;
     },
 
