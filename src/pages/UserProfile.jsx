1528a5cb6537315d2b73e0e24a77a6bdf1f87a89
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-07-07 14:16:49 +0530

    feat: Remove loader

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -3,7 +3,6 @@ import {
   Flex,
   useDisclosure,
   Heading,
-  CircularProgress,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -18,6 +17,7 @@ import {
   EditUserProfileModal,
 } from "../components";
 import { getSingleUser, getUserPosts } from "../services";
+import { useSelector } from "react-redux";
 
 const UserProfile = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -28,14 +28,15 @@ const UserProfile = () => {
   } = useDisclosure();
 
   const { username } = useParams();
+  const { posts } = useSelector((state) => state.auth);
+  const { users } = useSelector((state) => state.users);
   const [userProfile, setUserProfile] = useState(null);
   const [userPosts, setUserPosts] = useState(null);
-  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
-    getSingleUser(setUserProfile, username, setLoader);
-    getUserPosts(setUserPosts, username, setLoader);
-  }, [username]);
+    getSingleUser(setUserProfile, username);
+    getUserPosts(setUserPosts, username);
+  }, [username, posts, users]);
 
   return (
     <>
@@ -46,77 +47,69 @@ const UserProfile = () => {
       />
       <Header onOpen={onOpen} />
       <Box>
-        {loader ? (
-          <CircularProgress
-            isIndeterminate
-            color="brand.500"
-            position="fixed"
-            top="50%"
-            left="50%"
-            size="80px"
-            thickness="10px"
-          />
-        ) : (
-          <>
-            <Flex
-              bg="var(--bg-color)"
-              w="100%"
-              gap="10"
-              minHeight="100vh"
-              pl="6"
-              pr="6"
-            >
-              <SideNav onOpen={onOpen} />
-              <Flex
-                flexDirection="column"
-                gap="5"
-                flexGrow="1"
-                mt="6rem"
-                mr="2"
-                ml="2"
-              >
-                <UserProfileCard
-                  onOpenProfile={onOpenProfile}
-                  userProfile={userProfile}
-                  userPostsLength={userPosts?.length}
-                />
-                <Box w="100%">
-                  <Heading as="h3" size="md">
-                    Your Posts
-                  </Heading>
+        <Flex
+          bg="var(--bg-color)"
+          w="100%"
+          gap="10"
+          minHeight="100vh"
+          pl={{ base: "3", lg: "6" }}
+          pr={{ base: "3", lg: "6" }}
+        >
+          <SideNav onOpen={onOpen} />
+          <Flex
+            flexDirection="column"
+            gap="5"
+            flexGrow="1"
+            mt="6rem"
+            mr="2"
+            ml="2"
+          >
+            <UserProfileCard
+              onOpenProfile={onOpenProfile}
+              userProfile={userProfile}
+              userPostsLength={userPosts?.length}
+            />
+            <Box w="100%">
+              <Heading as="h3" size="md">
+                Your Posts
+              </Heading>
 
-                  {userPosts?.length !== 0 ? (
-                    <Flex flexDirection="column" gap="5" mt="2rem" mb="2rem">
-                      {userPosts?.map((post) => (
-                        <PostCard key={post._id} post={post} />
-                      ))}
-                    </Flex>
-                  ) : (
-                    <Flex w="50rem" justifyContent="center" alignItems="center">
-                      <Heading as="h3" size="md" textAlign="center">
-                        No posts to display, start posting and following other
-                        users to update your feed.
-                      </Heading>
-                    </Flex>
-                  )}
-                </Box>
-              </Flex>
-              <SuggestedUsersSidebar />
-            </Flex>
-            <Box
-              position="sticky"
-              bottom="0"
-              left="0"
-              right="0"
-              h="50px"
-              bgColor="brand.200"
-              display={{ base: "block", md: "none" }}
-              zIndex="2"
-            >
-              <MobileNav onOpen={onOpen} />
+              {userPosts?.length !== 0 ? (
+                <Flex
+                  flexDirection="column"
+                  gap="5"
+                  maxW="50rem"
+                  mt="2rem"
+                  mb="2rem"
+                >
+                  {userPosts?.map((post) => (
+                    <PostCard key={post._id} post={post} />
+                  ))}
+                </Flex>
+              ) : (
+                <Flex w="50rem" justifyContent="center" alignItems="center">
+                  <Heading as="h3" size="md" textAlign="center">
+                    No posts to display, start posting and following other users
+                    to update your feed.
+                  </Heading>
+                </Flex>
+              )}
             </Box>
-          </>
-        )}
+          </Flex>
+          <SuggestedUsersSidebar />
+        </Flex>
+        <Box
+          position="sticky"
+          bottom="0"
+          left="0"
+          right="0"
+          h="50px"
+          bgColor="brand.200"
+          display={{ base: "block", md: "none" }}
+          zIndex="2"
+        >
+          <MobileNav onOpen={onOpen} />
+        </Box>
       </Box>
     </>
   );
--- src/services/getUserPosts.js
+++ src/services/getUserPosts.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 
-const getUserPosts = async (setUserPosts, username, setLoader) => {
+const getUserPosts = async (setUserPosts, username) => {
   try {
-    setLoader(true);
     const response = await axios.get(`/api/posts/user/${username}`);
     setUserPosts(response.data.posts);
   } catch (error) {
     console.error(error);
-  } finally {
-    setLoader(false);
-  }
+  } 
 };
 
 export { getUserPosts };
