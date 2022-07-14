212fd10e37e9f1a1892e969f0847be21c00deca5
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-07-14 19:51:32 +0530

    feat: Add comment

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,11 +1,45 @@
-import { Avatar, Flex, Input, InputGroup, InputRightElement,IconButton } from "@chakra-ui/react";
+import { Avatar, Flex, Input, InputGroup, InputRightElement,IconButton, useToast } from "@chakra-ui/react";
 import {AiOutlineSend} from "react-icons/ai";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 
-const CommentInput = ()=>{
+const CommentInput = ({postId})=>{
 
-    const {user} = useSelector(state=>state.auth)
+    const {user, token} = useSelector(state=>state.auth);
+    const dispatch = useDispatch();
+    const toast = useToast();
+    const [commentData, setCommentData] = useState("");
+
+    const addCommentHandler = async () => {
+        if (commentData === "") {
+          toast({
+            description: "Comment cannot be empty",
+            status: "warning",
+            duration: 2000,
+            isClosable: true,
+          });
+        } else {
+          const response = await dispatch(
+            addComment({ postId, commentData, token })
+          );
+          if (response?.payload.status === 201) {
+            toast({
+              description: "Comment has been added",
+              status: "success",
+              duration: 2000,
+              isClosable: true,
+            });
+          } else {
+            toast({
+              description: `${response.payload.data.errors[0]}`,
+              status: "error",
+              duration: 2000,
+              isClosable: true,
+            });
+          }
+        }
+      };
+    
 
     return(
          <Flex gap='2' alignItems="center">
@@ -29,6 +63,8 @@ const CommentInput = ()=>{
                _focus={{
                    borderColor: "brand.500"
                }}
+               value={commentData}
+               onChange={(e) => setCommentData(e.target.value)}
              />
              <InputRightElement mr="2">
              <IconButton
@@ -49,6 +85,7 @@ const CommentInput = ()=>{
                 bgColor:"gray.100",
                 
              }}
+             onClick={addCommentHandler}
           >
 
           </IconButton>
